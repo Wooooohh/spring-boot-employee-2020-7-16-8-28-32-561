05f476ffc17f06920f7663926444cc8e8cc134d0
Wooooohh <1020227487@qq.com>
2020-08-01 22:10:29 +0800

    [CompanyIntegrationTest] [Jack] test: should_return_company_when_get_company_by_id_given_company_id

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
+    Company findByName(String name);
 }
--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.springbootemployee.integrationtest;
 
 import com.thoughtworks.springbootemployee.SpringBootEmployeeApplication;
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -9,6 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = {SpringBootEmployeeApplication.class})
 @AutoConfigureMockMvc
@@ -27,11 +36,21 @@ public class CompanyIntegrationTest {
   }
 
   @Test
-  void should_return_company_when_get_company_by_id_given_company_id(){}
+  void should_return_company_when_get_company_by_id_given_company_id() throws Exception {
+    // given
+    Company company = new Company("oocl");
+    companyRepository.save(company);
+    Company addedCompany = companyRepository.findByName("oocl");
+    // when
+    mockMvc
+            .perform(get("/companies/" + addedCompany.getCompanyId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("name").value("oocl"));
+  }
 
   @Test
-  void should_return_employees_when_get_employees_by_company_id_given_company_id()
-  {}
+  void should_return_employees_when_get_employees_by_company_id_given_company_id_and_two_employees_in_this_company() throws Exception {
+  }
 
   @Test
   void should_return_paged_companies_when_get_employees_by_page_given_page_info()
